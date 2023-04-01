a9105f5ee401ea360eea49d410d4a8c5717b34a8
taubenangriff <51975164+taubenangriff@users.noreply.github.com>
2023-04-01 21:50:43 +0200

    add factory for deep speech clients based on language

---
+++ soundsforanno.transcription/src/DeepSpeechFactory.cs
@@ -0,0 +1,41 @@
+﻿using DeepSpeechClient;
+using DeepSpeechClient.Interfaces;
+using SoundsForAnno.Serializable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundsForAnno.Transcription
+{
+    public class DeepSpeechFactory : IDeepSpeechFactory
+    {
+        private Dictionary<Language, IDeepSpeech> _clients; 
+
+        public DeepSpeechFactory() { 
+            _clients = new Dictionary<Language, IDeepSpeech>();
+
+            TryConfigure(Language.eng, "lib/english");
+            TryConfigure(Language.ger, "lib/german");
+            TryConfigure(Language.ger, "lib/french");
+        }
+
+        private void TryConfigure(Language lang, String file_base)
+        {
+            var base_model_name = $"{file_base}.pbmm";
+            if (!File.Exists(base_model_name))
+                return;
+            var client = new DeepSpeech(base_model_name);
+
+            var scorer_name = $"{file_base}.scorer";
+            if (File.Exists(scorer_name))
+                client.EnableExternalScorer(scorer_name);
+
+            _clients.Add(lang, client);
+
+        }
+
+        public IDeepSpeech Get(Language lang) => _clients[lang];
+    }
+}
---
+++ soundsforanno.transcription/src/IDeepSpeechFactory.cs
@@ -0,0 +1,16 @@
+﻿using DeepSpeechClient.Interfaces;
+using SoundsForAnno.Serializable;
+using SoundsForAnno.Transcription;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoundsForAnno.Transcription
+{
+    public interface IDeepSpeechFactory
+    {
+        IDeepSpeech Get(Language lang);
+    }
+}
